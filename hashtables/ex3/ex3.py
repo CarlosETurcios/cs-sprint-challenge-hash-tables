9372780e535b3dc70777436bf5141d02978d0bc1
CarlosETurcios <turcios50@gmail.com>
2020-11-09 19:09:06 -0800

    met mvp

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -5,14 +5,47 @@
 # if a number in one list is in the other list return that number a
 # as results
 
-dict = {}
-
 
 def intersection(arrays):
     """
     YOUR CODE HERE
     """
     # Your code here
+    # create a dict
+    intersect_dict = {}
+    results = []
+
+    # loop through array
+    for numberlist in arrays:
+        for nums in numberlist:
+            if nums not in intersect_dict:
+                intersect_dict[nums] = 0
+            else:
+                intersect_dict[nums] += 1
+                # print(intersect_dict)
+    for key in intersect_dict:
+        # print(intersect_dict)
+        if intersect_dict[key] == len(arrays) - 1:
+            results.append(key)
+            # print(intersect_dict)
+
+    return results
+
+    # without hash table
+    # results = arrays[0]
+    # output = []
+
+    # # loop through list
+    # for array in arrays:
+    #     for number in array:
+    #         if number in results:
+    #             output.append(number)
+    #     results = output
+    #     output = []
+
+    # check to see if a number exist in between multiple list of intergers
+    #
+    #
 
 
 if __name__ == "__main__":
@@ -21,5 +54,8 @@ if __name__ == "__main__":
     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    # arrays.append([5, 7, 8] + [1, 2, 3])
+    # arrays.append([8, 3, 6] + [1, 2, 3])
+    # arrays.append([9, 8, 4] + [1, 2, 3])
 
     print(intersection(arrays))
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,21 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # make a has table
+    # loop through the list of number to find the number with negetives in it
+    # put those numbers in the dict
+    # return results
+    negative_number_dict = {}
+    found_pairs = []
 
-    return result
+    for number in a:
+        if number < 0:
+            negative_number_dict[number] = number
+    for number in negative_number_dict:
+        if abs(number) in a:
+            found_pairs.append(abs(number))
+
+    return found_pairs
 
 
 if __name__ == "__main__":
